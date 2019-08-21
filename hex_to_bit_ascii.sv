7875059382c2ea5ba11d1ea928b881e645d6f289
nicolasukd <53022979+nicolasukd@users.noreply.github.com>
2019-08-21 15:42:58 -0400

    Add files via upload

---
+++ hex_to_bit_ascii.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 21.08.2019 15:49:30
+// Design Name: 
+// Module Name: hex_to_bit_ascii
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module hex_to_bit_ascii(
+
+    );
+endmodule
