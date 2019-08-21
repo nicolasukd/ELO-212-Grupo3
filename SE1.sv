7613e6bbf4abc5448760492827eef35550f55d67
nicolasukd <53022979+nicolasukd@users.noreply.github.com>
2019-08-21 00:33:58 -0400

    Add files via upload

--- SE1.sv
+++ SE1.sv
@@ -25,11 +25,11 @@ module SE1(
     input logic CPU_RESETN,UART_TXD_IN,
     output logic [7:0]AN,
     output logic CA,CB,CC,CD,CE,CF,CG,  
-    output logic [15:0]LED,
+    output logic [11:0]LED,
     output logic LED16_B,LED16_G,LED16_R
     );
     logic [7:0]rx_data,digitos;
-    logic rx_ready,clkout,trigger;
+    logic rx_ready,clkout,trigger,muestra_resultado;
     
     logic [15:0]op1,op2,op1_d,op2_d,cp1,cp2;
     logic [1:0]cmd;
@@ -42,7 +42,7 @@ module SE1(
     
     
     rx_control U1(.clk(CLK100MHZ),.reset(~CPU_RESETN),.rx_ready(rx_ready)
-    ,.rx_in_data(rx_data),.rx_out1_data(op1),.rx_out2_data(op2),.alu_ctrl(cmd),.leds(LED[11:0]),.trigger(trigger));
+    ,.rx_in_data(rx_data),.rx_out1_data(op1),.rx_out2_data(op2),.alu_ctrl(cmd),.leds(LED[11:0]),.trigger(trigger),.muestra_resultado(muestra_resultado));
     
     display  U2(.digitos(digitos),.an(AN),.sevenSeg({CA,CB,CC,CD,CE,CF,CG}),.clk(clkout),.reset(CPU_RESETN),
     .d1(cp1[15:12]),.d2(cp1[11:8]),.d3(cp1[7:4]),.d4(cp1[3:0]),.d5(cp2[15:12]),.d6(cp2[11:8]),.d7(cp2[7:4]),.d8(cp2[3:0]));
@@ -53,22 +53,40 @@ module SE1(
     unsigned_to_bcd U5(.clk(CLK100MHZ),.trigger(1),.in(op2),.bcd(op2_d));
     unsigned_to_bcd U6(.clk(CLK100MHZ),.trigger(1),.in(alu_hexa),.bcd(alu_d));
     
-    always_ff @(posedge CLK100MHZ)begin
-        /*if(trigger)begin
-            digitos = 8'b11110000;
-            op1 = alu_d;
-        end
-        else begin
-            digitos = 8'b11111111;
-            op1 = op1_d;
-        end*/
-    end
+   /* always_ff @(posedge CLK100MHZ)begin
+         case(muestra_resultado)
+            1:begin
+                digitos = 8'b11110000;
+                cp1 = alu_d;    
+            end
+            0:begin
+                digitos = 8'b11111111;
+                cp1 = op1_d;
+                cp2 = op2_d;
+            end
+        endcase
+    end*/
     
-    always_comb begin
-        digitos = 8'b11111111;
-        cp1 = op1;
-        cp2 = op2;
-        
+    always_comb begin  
+        case(muestra_resultado)
+            1:begin
+                digitos = 8'b11110000;
+                cp1 = alu_d;   
+                cp2 = 'd0; 
+            end
+            0:begin
+                digitos = 8'b11111111;
+                cp1 = op1_d;
+                cp2 = op2_d;
+            end
+            default:begin
+                digitos = 8'b11111111; 
+                cp1 = 'd0;
+                cp2 = 'd0;      
+            end
+        endcase 
+       
+       
         case(cmd)
             2'b00:begin
                 LED16_B = 1;
@@ -93,6 +111,13 @@ module SE1(
                 LED16_G = 1;
                 LED16_R = 1;
                 botones = 4'b0010;
+                
+            end
+            default:begin
+                LED16_B = 0;
+                LED16_G = 0;
+                LED16_R = 0;
+                botones = 4'b0000;
                 
             end
         endcase
